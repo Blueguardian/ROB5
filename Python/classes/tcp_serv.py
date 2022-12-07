bcdb1351896aff2de56ffdec3f8a1f6651261fc9
dyanavalerya <vacaru.diana.valeria@gmail.com>
2022-12-07 17:43:51 +0100

    changed server port

--- Python/classes/tcp_serv.py
+++ Python/classes/tcp_serv.py
@@ -5,7 +5,7 @@ class TCPServer:
     # If you pass an empty string, the server will accept connections on all available IPv4 interfaces.
     # HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     __HOST = ""
-    __PORT = 8998  # Port to listen on (non-privileged ports are > 1023)
+    __PORT = 15567  # Port to listen on (non-privileged ports are > 1023)
     __SIZE = 512
     __FORMAT = "utf-8"
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
